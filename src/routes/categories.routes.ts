5b8c0bad0dc533e8fe3b43ac9ed0eba373cbd5d6
Thiago <thiagofreitascarneiro@hotmail.com>
2022-03-18 21:51:12 -0300

    add SOLID in the Project

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import CategoriesRepository from '../repositories/CategoriesRepository';
+import CreateCategoryService from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -7,9 +8,17 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export default categoriesRoutes;
