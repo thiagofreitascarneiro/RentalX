0aed1920f2d5c4901a8da71c9dcfe396cb99eb0f
Thiago <thiagofreitascarneiro@hotmail.com>
2022-03-22 21:39:49 -0300

    create categories upload

---
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -0,0 +1,11 @@
+import { ImportCategoryController } from './ImportCategoryController';
+import { ImportCategoryUseCase } from './ImportCategoryUseCase';
+import CategoriesRepository from '../../repositories/implementations/CategoriesRepository';
+
+const categoriesRepository = CategoriesRepository.getInstance();
+const importCategoryUseCase = new ImportCategoryUseCase(categoriesRepository);
+const importCategoryController = new ImportCategoryController(
+  importCategoryUseCase,
+);
+
+export { importCategoryController };
